df4449f110bf4fba201cb6442e7716152158bca8
snehaa1989 <snehaa1989@github.com>
2023-07-21 16:06:36 +0530

    Added path to the server.

--- Utkarsh Rozgar/server/app.js
+++ Utkarsh Rozgar/server/app.js
@@ -14,6 +14,16 @@ const port = process.env.PORT || 5000;
 require('./Database/connection');
 // Your code
 
+    const path = require("path");
+    app.use(express.static(path.resolve(__dirname, 'client', 'build')));
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'),function (err) {
+            if(err) {
+                res.status(500).send(err)
+            }
+        });
+    })
+
 // Your code
 
 
