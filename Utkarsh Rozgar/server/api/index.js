561bc907e726c68ae82709ec7dd3722f270af0de
snehaa1989 <snehaa1989@github.com>
2023-07-21 17:07:36 +0530

    Changed files.

--- Utkarsh Rozgar/server/api/index.js
+++ Utkarsh Rozgar/server/api/index.js
@@ -13,17 +13,17 @@ const port = process.env.PORT || 5000;
 // configuring database
 require('../Database/connection');
 // Your code
-// if (process.env.NODE_ENV === "production") {
-//     const path = require("path");
-//     app.use(express.static(path.resolve(__dirname, 'client', 'build')));
-//     app.get("*", (req, res) => {
-//         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'),function (err) {
-//             if(err) {
-//                 res.status(500).send(err)
-//             }
-//         });
-//     })
-// }
+if (process.env.NODE_ENV === "production") {
+    const path = require("path");
+    app.use(express.static(path.resolve(__dirname, 'client', 'build')));
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'),function (err) {
+            if(err) {
+                res.status(500).send(err)
+            }
+        });
+    })
+}
 // Your code
 
 
